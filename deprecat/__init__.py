83e4549fb28a1ef30f5ae655713637f9c7de4f35
Meenal Jhajharia <meenal@mjhajharia.com>
2021-11-03 12:25:36 +0530

    Update __init__.py

--- deprecat/__init__.py
+++ deprecat/__init__.py
@@ -1,12 +1,12 @@
 """
-deprecat Library
+deprecat
 ==================
 
-Python ``@deprecat`` decorator to deprecate old python classes, functions or methods.
+``@deprecat`` decorator to deprecate python classes, functions, methods or kwargs.
 
 """
 
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 __author__ = u"Meenal Jhajharia <meenal@mjhajharia.com>"
 __credits__ = "(c) Meenal Jhajharia"
 
