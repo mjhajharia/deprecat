65c32fe87b0af88b28636e6fd6696d634d9bab53
Paul Haesler <linkjuggler@gmail.com>
2024-07-24 09:59:15 +1000

    Give up on 2.1.2 tag/release.  Prepare for a 2.1.3 release

--- deprecat/__init__.py
+++ deprecat/__init__.py
@@ -6,8 +6,8 @@ deprecat
 
 """
 
-__version__ = "2.1.2"
-__author__ = u"Meenal Jhajharia <meenal@mjhajharia.com> and contributors"
-__credits__ = "(c) Meenal Jhajharia and contributors"
+__version__ = "2.1.3"
+__author__ = u"Meenal Jhajharia <meenal@mjhajharia.com>, Paul Haesler <linkjuggler@gmail.com> and contributors"
+__credits__ = "(c) Meenal Jhajharia, Paul Haesler, and contributors"
 
 from deprecat.classic import deprecat
